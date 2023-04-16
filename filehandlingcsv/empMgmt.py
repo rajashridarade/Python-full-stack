df23b9f47c209b9a15dbd3db55f7cd4cb7572f16
navnath <navnath.lavhare11@gmail.com>
2023-04-16 12:56:44 +0530

    files

--- filehandlingcsv/empMgmt.py
+++ filehandlingcsv/empMgmt.py
@@ -46,3 +46,73 @@ class EmpMgmt:
 
         else:
             print("file is not present")
+
+    def deleteEmpbyid(id):
+        if os.path.exists("empdata.txt"):
+            with open('empdata.txt', "r") as fp:
+                allEmp = []
+                found = False
+                for emp in fp:
+                    try:
+                        emp.index(str(id), 0, 4)
+                    except:
+                        # no match
+                        allEmp.append(emp)
+                    else:
+                        # match
+                        found = True
+
+            print(allEmp)
+
+            if found:
+                with open("empdata.txt", 'w') as fp:
+                    for emp in allEmp:
+                        fp.write(emp)
+
+            else:
+                print("record not found")
+
+        else:
+            print("file is not present")
+
+    def editEmpbyid(id):
+        if os.path.exists("empdata.txt"):
+            with open('empdata.txt', "r") as fp:
+                allEmp = []
+                found = False
+                for emp in fp:
+                    try:
+                        emp.index(str(id), 0, 4)
+                    except:
+                        # no match
+                        pass
+                    else:
+                        # match
+                        found = True
+                        # str tp list
+                        emp = emp.split(",")
+                        ans = input("Do you want to change name(y/n)?")
+                        if ans.lower() == 'y':
+                            emp[1] = input("Enter the new name:")
+                        ans = input("Do you want to change Salary(y/n)?")
+                        if ans.lower() == 'y':
+                            emp[2] = float(input("Enter the new salary:"))
+                            emp[2] = str(emp[2]) + "\n"
+                            # convert list to string
+                            emp = ",".join(emp)
+
+                    finally:
+                        allEmp.append(emp)
+
+            print(allEmp)
+
+            if found:
+                with open("empdata.txt", 'w') as fp:
+                    for emp in allEmp:
+                        fp.write(emp)
+
+            else:
+                print("record not found")
+
+        else:
+            print("file is not present")
--- filehandlingcsv/main.py
+++ filehandlingcsv/main.py
@@ -8,6 +8,9 @@ if __name__ == "__main__":
         print("\t\t2.Display all emp")
         print("\t\t3.searchbyid emp")
         print("\t\t4.searchbyName emp")
+        print("\t\t5.Delete by id")
+        print("\t\t6.Edit by id")
+
         print("\t\t10.Exist")
         choice = int(input("Enter a choice :"))
         if choice == 1:
@@ -24,6 +27,12 @@ if __name__ == "__main__":
         elif choice == 4:
             name = input("Enter name :")
             EmpMgmt.searchEmpbyName(name)
+        elif choice == 5:
+            id = int(input("Enter id :"))
+            EmpMgmt.deleteEmpbyid(id)
+        elif choice == 6:
+            id = int(input("Enter id :"))
+            EmpMgmt.editEmpbyid(id)
         elif choice == 10:
             print("____________program end ----------------")
         else:
